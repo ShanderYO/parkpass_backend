0627fb28e36926006dedbc3d6bad8fe028641ae5
bugor <diman-mich@yandex.ru>
2019-10-07 13:00:00 +0300

    Fix bug with subscriptions

--- payments/tasks.py
+++ payments/tasks.py
@@ -36,7 +36,27 @@ def make_buy_subscription_request(subscription_id):
         subscription = RpsSubscription.objects.get(
             id=subscription_id
         )
-        subscription.request_buy()
+        order = Order.objects.get(
+            authorized = True,
+            subscription = subscription)
+
+        payments = TinkoffPayment.objects.filter(order=order)
+
+        if subscription.request_buy():
+            for payment in payments:
+                if payment.status == PAYMENT_STATUS_AUTHORIZED:
+                    order.confirm_payment(payment)
+                    return
+        else:
+            for payment in payments:
+                if payment.status == PAYMENT_STATUS_AUTHORIZED:
+                    request_data = payment.build_cancel_request_data()
+                    result = TinkoffAPI().sync_call(
+                        TinkoffAPI.CANCEL, request_data
+                    )
+                    logging.info("Cancel payment response: ")
+                    logging.info(str(result))
+                    return
 
     except ObjectDoesNotExist:
         get_logger().warn("Subscription does not found")
