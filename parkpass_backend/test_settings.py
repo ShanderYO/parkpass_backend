c730406eade94e0e9b9cd968897e7b276902bff4
Aly <dev@parkpass.ru>
2024-06-29 15:42:41 +0000

    init pytest

---
+++ parkpass_backend/test_settings.py
@@ -0,0 +1,24 @@
+try:
+    from .settings import *
+except Exception:
+    raise
+
+# MEDIA_ROOT = '/'
+
+# DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': "getvalet",
+#             'USER': "getvalet",
+#             'PASSWORD': "getvalet",
+#             'HOST': "pgtest",  # Set to empty string for localhost.
+#             'PORT': '5432',  # Set to empty string for default.
+#         }
+#     }
+
+
+# try:
+#     from .local_test_settings import *
+# except ImportError:
+#     pass
+
